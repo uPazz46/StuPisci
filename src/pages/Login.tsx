02b9ec63f7a86899a9382f7a639125062f346b94
uPazz46 <samuel2005ct@gmail.com>
2024-02-21 15:28:34 +0100

    Aggiunt foto

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 export default function Login() {
   return (
     <div className="  text-white h-screen bg-white flex justify-center items-center ">
+      <div className="bg-stupisci_b w-screen h-80"></div>
       <form className="font-font_login shadow_Login w-96 h-1/3 bg-stupisci_b flex-col flex justify-around items-center">
         <h1>BENVENUTO</h1>
         <input
